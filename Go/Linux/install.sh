cc47f79c14e6f67d0d378489a5fdec33f947e22d
HZH <42015599+capric98@users.noreply.github.com>
2019-10-01 00:20:44 +0800

    del sth

--- Go/Linux/install.sh
+++ Go/Linux/install.sh
@@ -10,7 +10,3 @@ mkdir $HOME/.go
 echo "export GOPROXY=https://mirrors.aliyun.com/goproxy/" >> ~/.profile
 echo "export GOPATH=$HOME/.go" >> ~/.profile
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
-
-echo "Install some utils..."
-apt-get update 1>/dev/null 2>&1
-apt-get install build-essential autoconf automake pkg-config
