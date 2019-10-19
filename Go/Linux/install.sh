fcb05726d4d42aeb377121b5ffb69c4cc75292ed
HZH <42015599+capric98@users.noreply.github.com>
2019-10-20 03:32:43 +0800

    Update install.sh

--- Go/Linux/install.sh
+++ Go/Linux/install.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-VERSION="1.13.1"
+VERSION="1.13.3"
 OS="linux"
 ARCH="amd64"
 
 wget https://dl.google.com/go/go${VERSION}.${OS}-${ARCH}.tar.gz
-tar -C /usr/local -xzf go${VERSION}.${OS}-${ARCH}.tar.gz
 
-mkdir $HOME/.go
-echo "export GOPROXY=https://mirrors.aliyun.com/goproxy/" >> ~/.profile
-echo "export GOPATH=$HOME/.go" >> ~/.profile
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
+if [[ -d /usr/local/go/ ]]; then
+    # Update.
+    echo "Golang exists:"
+    echo `go version`
+    echo "Updating..."
+    rm -rf /usr/local/go/
+else
+    # First time.
+    mkdir -p $HOME/.go
+    echo "export GOPATH=$HOME/.go" >> ~/.profile
+    echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
+fi
+
+tar -C /usr/local -xzf go${VERSION}.${OS}-${ARCH}.tar.gz
+echo "Golang installed:"
+echo `go version`
+
+echo -e "If you are using Golang in Mainland China, you could enable goproxy by:"
+echo -e "echo \"export GOPROXY=https://mirrors.aliyun.com/goproxy/\" >> ~/.profile"
+
+rm -rf go${VERSION}.${OS}-${ARCH}.tar.gz
