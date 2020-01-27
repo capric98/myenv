eac5a6218a40a86b3f652a5e6a7ad7170c7e3e51
capric98 <capric98@users.noreply.github.com>
2020-01-27 07:19:41 +0000

    Update golang version by actions.

--- Go/Linux/install.sh
+++ Go/Linux/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Usage: curl -fsSL https://raw.githubusercontent.com/capric98/myenv/master/Go/Linux/install.sh | bash
 
-VERSION=`curl -fsSL https://raw.githubusercontent.com/capric98/myenv/master/Go/Linux/version.py | python3`
+VERSION=1.13.6
 OS="linux"
 ARCH="amd64"
 if [[ $(uname --m) == "aarch64" ]]; then
