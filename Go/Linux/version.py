7db9e9a29f5d3eb036542fc964a8cba15dcef58a
HZH <42015599+capric98@users.noreply.github.com>
2020-07-01 13:32:53 +0800

    Update version.py

--- Go/Linux/version.py
+++ Go/Linux/version.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 #coding=utf-8
 import requests
+import re
 
 r = requests.get("https://golang.org/dl").text
 src_pos = r.find("src.tar.gz")
@@ -8,5 +9,4 @@ src_pos = r.find("src.tar.gz")
 link = r[src_pos-40:src_pos+10]
 link = link[link.find("\"")+1:]
 
-version_pos = link.find("go/go")+5
-print(link[version_pos:link.find("src")-1], end="")
+print(re.search("go[0-9].*?src", link).group(0)[2:-4])
