8f1b1a17f7abc19ac89e0f9e51636a86c23be826
Actions <capric98@users.noreply.github.com>
2024-08-07 01:02:18 +0000

    Update golang version by actions.

--- Go/Linux/install.sh
+++ Go/Linux/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Usage: curl -fsSL https://raw.githubusercontent.com/capric98/myenv/master/Go/Linux/install.sh | bash
 
-VERSION=1.22.5
+VERSION=1.22.6
 OS="linux"
 ARCH="amd64"
 if [[ $(uname --m) == "aarch64" ]]; then
